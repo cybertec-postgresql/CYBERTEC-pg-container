727d4183a9202b8d660aeb4899404b4306668ef3
matthias <matthias@swobblmail.eu>
2024-07-29 09:55:02 +0200

    add method to check pgversion and stanzaversion and upgrade stanza if needed

--- scripts/postgres/init_pgbackrest.sh
+++ scripts/postgres/init_pgbackrest.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # Check Structure
+if [ -d "/home/postgres/pgdata/pgbackrest/log" ]; then
+    echo "Skip: Folder structure already exists ... "
+else
+    mkdir -p /home/postgres/pgdata/pgbackrest/log
+    echo "Created: Folder structure was created"
+fi
+if [ -d "/home/postgres/pgdata/pgbackrest/spool-path" ]; then
+    echo "Skip: Spool-Folder already exists ... "
+else
+    mkdir -p /home/postgres/pgdata/pgbackrest/spool-path
+    echo "Created: Spool-Folder was created"
+fi
+
 if [ "$REPO_HOST" == true ]; then
     echo "pgBackRest is running in tls-mode. Skip initialisation on this Container."
 else
     echo "Initialise pgBackRest ... "
-    if [ -d "/home/postgres/pgdata/pgbackrest/log" ]; then
-        echo "Skip: Folder structure already exists ... "
-    else
-        mkdir -p /home/postgres/pgdata/pgbackrest/log
-        echo "Created: Folder structure was created"
-    fi
-    if [ -d "/home/postgres/pgdata/pgbackrest/spool-path" ]; then
-        echo "Skip: Spool-Folder already exists ... "
-    else
-        mkdir -p /home/postgres/pgdata/pgbackrest/spool-path
-        echo "Created: Spool-Folder was created"
-    fi
     # Create Stanza and run Init-Backup
     stanza=$(pgbackrest info --output=json)
     if [ "$stanza" == "[]" ]; then
@@ -29,4 +30,20 @@ else
             echo "Finished: pgBackRest is ready for use"
         fi
     fi
+fi
+
+# Check if stanza needs an upgrade
+
+# Get current used postgresql version
+pg_version=$(psql -V | awk '{print $3}' | cut -d '.' -f 1)
+
+# Get current Stanza version from pgbackrest info
+stanza_version=$(echo $(pgbackrest info --stanza=db --output=json) | jq -r '.[0].db[0].version')
+
+# Check if current postgresql versions mapped to current stanza version. If not do a stanza upgrade 
+if [ "$pg_version" != "$stanza_version" ]; then
+  echo "Stanza version matches another PostgreSQL version: A Stanza upgrade is required to ensure that pgBackRest can work with the current version of PostgreSQL"
+  pgbackrest stanza-upgrade --stanza=db
+else
+  echo "Stanza is up to date. No change required."
 fi

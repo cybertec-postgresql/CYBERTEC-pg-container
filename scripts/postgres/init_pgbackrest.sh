51776f3674492995a3d967b3fe93714069fb8d97
matthias <matthias@swobblmail.eu>
2024-07-29 19:01:16 +0200

    updated major-upgrade

--- scripts/postgres/init_pgbackrest.sh
+++ scripts/postgres/init_pgbackrest.sh
@@ -33,17 +33,4 @@ else
 fi
 
 # Check if stanza needs an upgrade
-
-# Get current used postgresql version
-pg_version=$(psql -V | awk '{print $3}' | cut -d '.' -f 1)
-
-# Get current Stanza version from pgbackrest info
-stanza_version=$(echo $(pgbackrest info --stanza=db --output=json) | jq -r '.[0].db[0].version')
-
-# Check if current postgresql versions mapped to current stanza version. If not do a stanza upgrade 
-if [ "$pg_version" != "$stanza_version" ]; then
-  echo "Stanza version matches another PostgreSQL version: A Stanza upgrade is required to ensure that pgBackRest can work with the current version of PostgreSQL"
-  pgbackrest stanza-upgrade --stanza=db
-else
-  echo "Stanza is up to date. No change required."
-fi
+source /scripts/postgres/stanza_upgrade.sh

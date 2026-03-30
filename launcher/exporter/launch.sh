df2fb14c1af75258e1dbb8a0afbe6e2af4a004c0
matthias <matthias@swobblmail.eu>
2026-03-30 10:50:22 +0200

    fix situation with recovery.signal after create_replica, add sed to exporter and Update etcd

--- launcher/exporter/launch.sh
+++ launcher/exporter/launch.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
-# Start the Exporter
 
 EXPORTER_DIR='/postgres_exporter'
-QUERIE_FOLDER="$EXPORTER_DIR/queries"
-VERSION_SPECIFIC_FOLDER="$EXPORTER_DIR/version_specific/$PGVERSION"
-FILES="${QUERIE_FOLDER}/*"
 TEMP_QUERY_FILE='/tmp/cpo_queries.yaml'
+CONFIG_FILE='/tmp/cpo_exporter_configuration.yaml'
 
-> $TEMP_QUERY_FILE
+# query folder definition
+QUERY_DIRS=(
+  "$EXPORTER_DIR/queries"
+  "$EXPORTER_DIR/version_specific/$PGVERSION"
+  "$EXPORTER_DIR/custom_queries"
+)
 
-for file in $FILES
-do
-  if [[ -f "$file" ]]; then
-    cat "$file" >> "$TEMP_QUERY_FILE"
-  else
-    echo "Query file $file does not exist (it should).."
-    exit 1
+# create files for config and queries
+> "$TEMP_QUERY_FILE"
+echo "{}" > "$CONFIG_FILE"
+
+for dir in "${QUERY_DIRS[@]}"; do
+  if [ -d "$dir" ] && [ "$(ls -A "$dir" 2>/dev/null)" ]; then
+    for file in "$dir"/*; do
+      if [[ -f "$file" ]]; then
+        cat "$file" >> "$TEMP_QUERY_FILE"
+        echo "" >> "$TEMP_QUERY_FILE"
+      fi
+    done
   fi
 done
 
-# VERSION_FILES="${VERSION_SPECIFIC_FOLDER}/*"
-# for version_file in $VERSION_FILES
-# do
-#   if [[ -f "$version_file" ]]; then
-#     cat "$version_file" >> "$TEMP_QUERY_FILE"
-#   else
-#     echo "Version-specific query file $version_file does not exist for PGVERSION $PGVERSION (it should).."
-#     exit 1
-#   fi
-# done
+# Check if query file is empty
+if [ ! -s "$TEMP_QUERY_FILE" ]; then
+  echo "INFO: queries.yaml is empty"
+fi
 
-/bin/postgres_exporter --extend.query-path=/tmp/cpo_queries.yaml #--auto-discover-databases
+echo "Start postgres_exporter..."
+/bin/postgres_exporter \
+  --config.file="$CONFIG_FILE" \
+  --extend.query-path="$TEMP_QUERY_FILE"
